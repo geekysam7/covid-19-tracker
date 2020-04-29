1e59b536eab97f1e4394cfb4d451e7df7d672bae
Sameer Utradhi <geekysam7@gmail.com>
2020-04-30 05:19:29 +0530

    Updates

---
+++ precache-manifest.b6abc50a251740cf8783ef75f6f9b9d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43ba5b8eeee64c0a2b5f090f986558ec",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "826f98660792e144f8a2",
+    "url": "/covid-19-tracker/static/css/main.3d740564.chunk.css"
+  },
+  {
+    "revision": "7683adb33fa0826b9976",
+    "url": "/covid-19-tracker/static/js/2.36ba73b8.chunk.js"
+  },
+  {
+    "revision": "13767bda7e58db6e95d0e3d841e04d61",
+    "url": "/covid-19-tracker/static/js/2.36ba73b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "826f98660792e144f8a2",
+    "url": "/covid-19-tracker/static/js/main.653d3a7a.chunk.js"
+  },
+  {
+    "revision": "7d0f1410831a634241b4",
+    "url": "/covid-19-tracker/static/js/runtime-main.036fad25.js"
+  }
+]);
